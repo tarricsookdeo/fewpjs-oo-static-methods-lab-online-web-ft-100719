4e37808361f9fffeb4ccd5aafd3cd0c48780a5e2
Tarric Sookdeo <tarricsookdeo@outlook.com>
2020-02-09 01:11:27 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-.' ]+/g, "");
+  }
+
+  static titleize(string) {
+    const exceptions = [
+      "the",
+      "a",
+      "an",
+      "but",
+      "of",
+      "and",
+      "for",
+      "at",
+      "by",
+      "from"
+    ];
+    return string
+      .split(" ")
+      .map(word => {
+        if (
+          !exceptions.some(exception => exception === word) ||
+          word === string.charAt(0)
+        )
+          return this.capitalize(word);
+        else return word;
+      })
+      .join(" ");
+  }
 }
